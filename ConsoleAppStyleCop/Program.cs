e74031fabb247d83860cfc3ad077206b46f3e247
sumeetbajaj9 <sumeetbajaj9@gmail.com>
2022-03-14 16:04:17 +0530

    firstpush

---
+++ ConsoleAppStyleCop/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppStyleCop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
